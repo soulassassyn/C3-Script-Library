b64fe90a069942ad5906713a87c3d3dbfbec107b
Kyle H <soulassassyn@gmail.com>
2023-04-10 15:50:48 -0600

    normalization

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
-
-// Import any other script files here, e.g.:
-// import * as myModule from "./mymodule.js";
+import { SpriteGrid } from "./spriteGrid.js";
+import { initializeSpriteGrid } from "./spriteGrid.js";
+import { MouseHoverEffect } from "./mouseHover.js";
+import { EnemySpawner } from './enemySpawner.js';
 
 runOnStartup(async runtime =>
 {
@@ -15,11 +16,30 @@ async function OnBeforeProjectStart(runtime)
 	// Code to run just before 'On start of layout' on
 	// the first layout. Loading has finished and initial
 	// instances are created and available to use here.
+
 	
+	initializeSpriteGrid(runtime, runtime.objects.rarityBox.name, 3, 1, 40);
+	initializeSpriteGrid(runtime, runtime.objects.gridShip.name, 3, 1, 40);
+	
+	// List of objects effected by the MouseHoverEffect class
+	const mouseHoverShip = new MouseHoverEffect(runtime, "gridShip", 2.3, 2);
+	const mouseHoverWeapon = new MouseHoverEffect(runtime, "gridWeapon", 2.3, 2);
+	const mouseHoverEngine = new MouseHoverEffect(runtime, "gridEngine", 2.3, 2);
+	
+	// Enemy spawner
+	const enemySpawner = new EnemySpawner(runtime);
+	runtime.enemySpawner = enemySpawner;
+	// Array of EnemySpawner objects for positional tracking
+	const enemyInstances = [];
+	runtime.enemyInstances = enemyInstances;
+
 	runtime.addEventListener("tick", () => Tick(runtime));
 }
 
 function Tick(runtime)
 {
 	// Code to run every tick
+	for (const enemyData of runtime.enemyInstances) {
+        runtime.enemySpawner.updateAttachments(enemyData.enemyInstance, enemyData.enemyWeapon, enemyData.enemyEngine);
+    }
 }
